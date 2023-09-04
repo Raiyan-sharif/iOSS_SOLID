9ea77d1e6dfd419ed3ea39e16e7320be54cbedcb
Raiyan Sharif <raiyan.sharif.1234@gmail.com>
2023-09-04 22:13:39 +0600

    New Project Little Lemon added

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by raiyan sharif on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

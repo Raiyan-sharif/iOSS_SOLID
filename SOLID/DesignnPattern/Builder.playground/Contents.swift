5995c607f44e042ff77036bbc50f5728f71bf526
Raiyan-sharif <raiyan.sharif.1234@gmail.com>
2024-03-27 11:33:46 +0600

    Builder

--- SOLID/DesignnPattern/Builder.playground/Contents.swift
+++ SOLID/DesignnPattern/Builder.playground/Contents.swift
@@ -29,6 +29,7 @@ class HtmlElement: CustomStringConvertible
             result += e.description(indent+1)
         }
         result += "\(i)</\(name)>\n"
+        return result
 
     }
     public var description: String{
@@ -51,6 +52,13 @@ class HtmlBuilder: CustomStringConvertible{
         root.elements.append(e)
     }
 
+    func addChildFluent(name: String, text: String) -> HtmlBuilder
+    {
+        let e = HtmlElement(name: name, text: text)
+        root.elements.append(e)
+        return self
+    }
+
     var description: String{
         return root.description
     }
@@ -60,14 +68,169 @@ class HtmlBuilder: CustomStringConvertible{
     }
 }
 
+class Person: CustomStringConvertible
+{
+    // address
+    var streetAddress = "", postcode = "", city = ""
+
+    // employment
+    var companyName = "", position = ""
+    var annualIncome = 0
+
+    var description: String
+    {
+        return "I live at \(streetAddress), \(postcode), \(city). " + "I work at \(companyName) as \(position) earning \(annualIncome)"
+    }
+}
+
+class PersonBuilder
+{
+    var person = Person()
+    var lives : PersonAddressBuilder
+    {
+        return PersonAddressBuilder(person)
+    }
+    var works: PersonJobBuilder
+    {
+        return PersonJobBuilder(person)
+    }
+
+    func build() -> Person
+    {
+        return person
+    }
+
+}
+
+class PersonJobBuilder : PersonBuilder{
+    internal init(_ person: Person) {
+        super.init()
+        self.person = person
+    }
+
+    func at(_ companyName: String) -> PersonJobBuilder
+    {
+        person.companyName = companyName
+        return self
+    }
+
+    func asA(_ position: String) -> PersonBuilder
+    {
+        person.position = position
+        return self
+    }
+
+    func earning(_ annualIncome: Int) -> PersonBuilder
+    {
+        person.annualIncome = annualIncome
+        return self
+    }
+}
+
+class PersonAddressBuilder: PersonBuilder{
+
+    internal init(_ person: Person)
+    {
+        super.init()
+        self.person = person
+    }
+
+    func at(_ streetAddress: String) -> PersonAddressBuilder
+    {
+        person.streetAddress = streetAddress
+        return self
+    }
+
+    func withPostcode(_ postcode: String) -> PersonAddressBuilder
+    {
+        person.postcode = postcode
+        return self
+    }
+
+    func inCity(_ city: String) -> PersonAddressBuilder
+    {
+        person.city = city
+        return self
+    }
+
+}
+
+class Field : CustomStringConvertible
+{
+  var type: String = ""
+  var name: String = ""
+  init(called name: String, ofType type: String)
+  {
+    self.name = name
+    self.type = type
+  }
+  public var description: String
+  {
+    return "var \(name): \(type)"
+  }
+}
+
+class Class : CustomStringConvertible
+{
+  var name = ""
+  var fields = [Field]()
+
+  public var description: String
+  {
+    var s = ""
+    s.append("class \(name)\n{\n")
+    for f in fields
+    {
+      s.append("  \(f)\n")
+    }
+    s.append("}\n")
+    return s
+  }
+}
+
+class CodeBuilder : CustomStringConvertible
+{
+  private var theClass = Class()
+
+  init(_ rootName: String)
+  {
+    theClass.name = rootName
+  }
+
+  func addField(called name: String, ofType type: String) -> CodeBuilder
+  {
+    theClass.fields.append(Field(called: name, ofType: type))
+    return self
+  }
+
+  public var description: String
+  {
+    return theClass.description
+  }
+}
 
 func main(){
 
-    let builder = HtmlBuilder(rootName: "ul")
-    builder.addChild(name: "li", text: "hello")
-    builder.addChild(name: "li", text: "world")
-    print(builder)
+//    let builder = HtmlBuilder(rootName: "ul")
+//    builder.addChildFluent(name: "li", text: "hello")
+//        .addChild(name: "li", text: "world")
+//    print(builder)
 
+
+    let pb = PersonBuilder()
+
+    let p = pb.lives
+        .at("123 London Road")
+        .inCity("London")
+        .withPostcode("SW12BC")
+        .works.at("BJIT")
+        .asA("SSE")
+        .works.earning(123322)
+        .build()
+    print(p)
+
+    var cb = CodeBuilder("Person").addField(called: "name", ofType: "String").addField(called: "age", ofType: "Int");
+    print(cb.description);
 }
 
 main()
